a98a865529e4b145b21e3133f91bfde66bca01da
gaoxiu <gaoxiu333@gmail.com>
2023-11-21 22:10:27 +0800

    feat: fn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // source https://github.com/infinitered/apisauce/tree/master
 
 import axios from "axios";
- 
+
 export default async function (url: string) {
   const response = await axios.get(url);
   return response.data;
@@ -16,4 +16,23 @@ const DEFAULT_CONFIG = {
   timeout: 0,
 };
 
-
+const create = (config) => {
+  const headers = {
+    ...(config.headers || {}),
+  };
+  let instance;
+  if (config.axiosInstance) {
+    instance = config.axiosInstance;
+  } else {
+    const configWithoutHeaders = { ...config, heaer: undefined };
+    const combinedConfig = {
+      ...DEFAULT_CONFIG,
+      ...configWithoutHeaders,
+    };
+    instance = axios.create(combinedConfig);
+  }
+  const monitors = [];
+  const addMonitor = (monitor) => {
+    monitor.push(monitor);
+  };
+};
